0a3736f937bb0bf05a381becf66aeb3995c0f3fa
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2021-11-13 18:14:15 +0530

    Home Page Added

--- plannr_app/src/App.js
+++ plannr_app/src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/Navbar/NavBar";
+import Herobox from "./components/Herobox/Herobox";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Herobox />
     </div>
   );
 }
