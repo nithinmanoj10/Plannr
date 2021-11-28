953d6fdc05a44b66e50973708b705056bc2c09d7
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2021-11-28 21:09:45 +0530

    :sparkles: Started with Dashboard

--- plannr_app/src/components/Dashboard/Dashboard.js
+++ plannr_app/src/components/Dashboard/Dashboard.js
@@ -1,11 +1,41 @@
-import React from "react";
-import Calendar from "./Calendar";
+import * as React from "react";
+import plannrLogo from "../../images/plannr_logo.svg";
+import { useNavigate } from "react-router-dom";
+import Button from "../Buttons/Button";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import { deepOrange, deepPurple } from "@mui/material/colors";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/");
+  }
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Calendar />
+    <div className="dashboard--container">
+      <div className="dashboard__nav-bar">
+        <a href="javascript:void(0)" onClick={handleClick}>
+          <img
+            src={plannrLogo}
+            alt="Plannr Logo"
+            className="dashboard__nav-bar__logo"
+          />
+        </a>
+        <div className="user-info">
+          <div className="user-info__avatar">
+            <Stack direction="row" spacing={2}>
+              <Avatar sx={{ bgcolor: deepOrange[500] }}>JD</Avatar>
+            </Stack>
+          </div>
+          <div className="user-info__name">John Doe</div>
+        </div>
+        <Button text="Logout" type="button__solid" />
+      </div>
+      <div className="dashboard__left-info">Calendar</div>
+      <div className="dashboard__middle-info">Time Table</div>
+      <div className="dashboard__right-info">Courses</div>
     </div>
   );
 }
