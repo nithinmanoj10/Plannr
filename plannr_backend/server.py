e607788691e9f7e72a7ba1287388797d836cdbe8
varghese1508 <55691245+varghese1508@users.noreply.github.com>
2021-11-29 23:44:37 +0530

    installed psycopg2 binary, added connect TEST

--- plannr_backend/server.py
+++ plannr_backend/server.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
+def connectDB_TEST():
+    engine = create_engine("postgresql://postgres:Alan200!@localhost:5432/plannr")
+    db = scoped_session(sessionmaker(bind=engine))
+    db.execute("CREATE TABLE TEST (id INTEGER NOT NULL, name VARCHAR, PRIMARY KEY (id));")
+    db.commit()
+    db.close()
+
 @app.route("/test")
 def test():
+    connectDB_TEST()
     return jsonify({"connTest" : ["this", "is", "a", "python", "object"]})
 
 if __name__ == "__main__":
