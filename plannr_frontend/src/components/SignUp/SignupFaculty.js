33f83ff62dcef64738a42b8dd1aeb39d99285065
akshaykbiju <akshay_b190803cs@nitc.ac.in>
2021-11-30 19:00:33 +0400

    added signUp and Login Page

---
+++ plannr_frontend/src/components/SignUp/SignupFaculty.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./login.css"
+import SignupImg from './SignupImg.svg'
+function Signup() {
+ 
+    return (
+        <div className="parent">
+            <div className="image">
+                <img src={SignupImg} alt="SignupImg" />
+            </div>
+            <div className="container">
+                <div className="header">Sign Up - Student</div>
+                <div className="form-group">
+                    <label>UserId: </label>
+                    <input name="userId" placeholder="userId" type="text" className="input-field" required></input>
+                </div>
+                <div className="form-group">
+                    <label>Password: </label>
+                    <input name="password" placeholder="password" type="password"  className="input-field" required></input>
+                </div>
+               
+                <div className="form-group">
+                    <label>Email: </label>
+                    <input name="email" placeholder="email" type="text"  className="input-field" required></input>
+                </div>
+                <div className="form-group">
+                    <label>DOB: </label>
+                    <input name="dob" placeholder="DateOfBirth" type="text"  className="input-field" required></input>
+                </div>
+                <div className="form-group">
+                    <label>MobileNo: </label>
+                    <input name="mobileno" placeholder="MobileNumber" type="text"  className="input-field" required></input>
+                </div>
+                <div className="form-group">
+                    <label>Batch: </label>
+                    <input name="mobileno" placeholder="MobileNumber" type="text"  className="input-field" required></input>
+                </div>
+                <div className="buttondiv">
+                <button type="submit">Submit</button>
+                </div>
+                <a href="#" className="signup">Sign Up - Student</a>
+                <a href="#" className="signup">Already have an account?
+                Login Here</a>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Signup
